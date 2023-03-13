3f8ed096a93a8bf33fb21eaec8189dd15a5b2b80
CodeX1991 <oghenewonaholy@gmail.com>
2023-03-13 15:58:48 -0700

    Fix the case of negative

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     idx = len(my_list) - 1
-    for n in my_list:
-        if idx >= 0:
-            print("{:d}".format(my_list[idx]))
+
+    while idx >= 0:
+        print("{:d}".format(my_list[idx]))
         idx = idx - 1
