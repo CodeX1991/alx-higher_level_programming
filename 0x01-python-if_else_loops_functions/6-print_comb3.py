122d167b493f70673483a50df623ffdc6dccaee4
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-08 15:02:45 -0700

    Add program that prints all possible different combinations of two digits and function that checks for lowercase character

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+j = 0
+
+for n in range(0, 9):
+    j = j + 1
+    for m in range(j, 10):
+        if n == 8 and m == 9:
+            print("{}{}".format(n, m))
+        else:
+            print("{}{}".format(n, m), end=", ")
+print()
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def islower(c):
+    """A function that check for lowercase after"""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    return False
