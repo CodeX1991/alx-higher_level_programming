7730f85a4b357e933340eb838105c946365a09cf
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-09 07:19:19 -0700

    Fix the str =  error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,16 +2,10 @@
 
 def uppercase(str):
     """Prints str in uppercases"""
-    sep = ""
-    strLen = len(str)
-    count = 0
-    for ch in str:
-        if count == strLen - 1:
-            sep = '\n'
-        if strLen == 0:
-            ch = ""
-        if ord(ch) >= 97 and ord(ch) <= 122:
-            print("{}".format(chr(ord(ch) - 32)), end=sep)
-        else:
-            print("{}".format(ch), end=sep)
-        count = count + 1
+    newStr = ""
+    for ch in range(len(str)):
+        if ord(str[ch]) >= 97 and ord(str[ch]) <= 122:
+            newStr += chr(ord(str[ch]) - 32)
+            continue
+        newStr += str[ch]
+    print("{}".format(newStr))
