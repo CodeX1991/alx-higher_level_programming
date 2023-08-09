c5a68a47853cc3c967392a496f512afc3789b31c
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-09 07:52:59 -0700

    Fix print issues

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,11 +3,11 @@
 def fizzbuzz():
     """ Prints the numbers from 1 to 100 separated by a space """
     for n in range(1, 101):
-        if n % 3 == 0:
+        if n % 3 == 0 and n % 5 != 0:
             print("{}".format("Fizz"), end=" ")
-        elif n % 5 == 0:
+        elif n % 5 == 0 and n % 3 != 0:
             print("{}".format("Buzz"), end=" ")
-        elif n % 3 == 0 and n % 5 == 0:
+        elif n % 15 == 0:
             print("{}".format("FizzBuzz"), end=" ")
         else:
             print("{}".format(n), end=" ")
