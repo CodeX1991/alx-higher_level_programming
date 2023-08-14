86ad0026ffd01f738621bd7c7358140c9069e488
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-14 08:31:24 -0700

    Fix emtpy entering

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -19,3 +19,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tuple_b
     if len(tuple_b) == 0:
         return tuple_a
+    if len(tuple_a) == 0 and len(tuple_b) == 0:
+        return 0, 0
