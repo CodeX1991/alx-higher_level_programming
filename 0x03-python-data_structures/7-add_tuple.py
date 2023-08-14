587200cdfcb03633db4535e56db1ba17b3a27802
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-14 08:36:56 -0700

    Fix empty entering

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,15 +9,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     Return:
         sum of the 2 tuples
     """
-    if len(tuple_a) < 2 and len(tuple_a) != 0:
-        return (tuple_a[0] + tuple_b[0], tuple_b[1])
-    if len(tuple_b) < 2 and len(tuple_b) != 0:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1])
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    if len(tuple_a) == 0:
-        return tuple_b
-    if len(tuple_b) == 0:
-        return tuple_a
-    if len(tuple_a) == 0 and len(tuple_b) == 0:
-        return 0, 0
+    if tuple_a or tuple_b:
+        if len(tuple_a) < 2 and len(tuple_a) != 0:
+            return (tuple_a[0] + tuple_b[0], tuple_b[1])
+        if len(tuple_b) < 2 and len(tuple_b) != 0:
+            return (tuple_a[0] + tuple_b[0], tuple_a[1])
+        if len(tuple_a) >= 2 and len(tuple_b) >= 2:
+            return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+        if len(tuple_a) == 0:
+            return tuple_b
+        if len(tuple_b) == 0:
+            return tuple_a
