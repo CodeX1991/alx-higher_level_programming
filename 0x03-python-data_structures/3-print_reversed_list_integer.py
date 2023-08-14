ce93f0b4e33a44cad42ee89fcdcb325921860d26
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-14 04:19:42 -0700

    Fix testcase of an empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,6 +6,7 @@ def print_reversed_list_integer(my_list=[]):
     Args:
         my_list: the given list to work with
     """
-    for i in range(1, (1 + len(my_list))):
-        i = -i
-        print("{:d}".format(my_list[i]))
+    if my_list:
+        for i in range(1, (1 + len(my_list))):
+            i = -i
+            print("{:d}".format(my_list[i]))
