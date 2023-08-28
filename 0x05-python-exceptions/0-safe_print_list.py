e31ab96d50b6086fe0ec0c49e1ebc43018b52e47
CodeX1991 <oghenewonaholy@gmail.com>
2023-08-28 04:12:36 -0700

    Fix newline error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -18,8 +18,10 @@ def safe_print_list(my_list=[], x=0):
         if x > size_of_list:
             x = size_of_list
         for i in range(0, x):
-            print("{}".format(my_list[i]), end="")
-        print('\n')
+            if i == x - 1:
+                print("{}".format(my_list[i]))
+            else:
+                print("{}".format(my_list[i]), end="")
         return x
     except Exception:
         pass
