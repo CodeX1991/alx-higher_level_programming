8e40bed77e6562d838029bcda4b6fb0d94b04edb
CodeX1991 <oghenewonaholy@gmail.com>
2023-09-06 02:15:07 -0700

    Add  a function magic_string() that returns a string “BestSchool” n times the number of the iteration (see code)

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(strin=[]):
+    strin.append("BestSchool")
+    return ', '.join(strin)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+class LockedClass(object):
+    """A class with restricted class attribute"""
+    __slots__ = 'first_name'
