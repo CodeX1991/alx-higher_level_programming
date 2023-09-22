a329b8e4b5a3bd7305e2267121a75e1ee7eb0623
CodeX1991 <oghenewonaholy@gmail.com>
2023-09-22 16:56:41 -0700

    Add a the class Rectangle by adding the public method def display(self): that prints in stdout the Rectangle instance with the character # - you don’t need to handle x and y her

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,12 @@ class Rectangle(Base):
     def area(self):
         """Return the area of the rectangle"""
         return self.__width * self.__height
+
+    def display(self):
+        """Prints in stdout the Rectangle instance with character #"""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                if j == self.__width - 1:
+                    print('#')
+                else:
+                    print('#', end="")
