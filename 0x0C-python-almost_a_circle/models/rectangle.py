3fe8138e58436828d77b459700b0f6ad46ac3c43
CodeX1991 <oghenewonaholy@gmail.com>
2023-09-22 15:19:34 -0700

    Update: setter route and getsetter

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,3 +15,43 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+
+    @property
+    def width(self):
+        """Retrieve the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, param):
+        """Set the width"""
+        self.__width = param
+
+    @property
+    def height(self):
+        """Retrieve the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, param):
+        """set the height"""
+        self.__height = param
+
+    @property
+    def x(self):
+        """Retrieve the x"""
+        return self.__x
+
+    @x.setter
+    def x(self, param):
+        """set the height"""
+        self.__x = param
+
+    @property
+    def y(self):
+        """Retrieve the height"""
+        return self.__y
+
+    @y.setter
+    def y(self, param):
+        """set the height"""
+        self.__y = param
