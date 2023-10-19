2f1ce62ac0f3df4444ebdf86ffb31950c87f9867
CodeX1991 <oghenewonaholy@gmail.com>
2023-10-18 17:43:43 -0700

    Fix order error

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- displays the number of recods with equal score on your database --
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY number DESC
