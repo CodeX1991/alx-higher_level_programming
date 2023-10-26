6b7076717bd38b4e2d382821c5a37fe4b721bc44
CodeX1991 <oghenewonaholy@gmail.com>
2023-10-26 02:56:24 -0700

    Add a script that lists all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- list all shows from hbtn_0d_tvshows_rate --
-SELECT title SUM(rate) AS rating
+SELECT title, SUM(rate) AS rating
 FROM tv_shows
 INNER JOIN tv_show_ratings
 ON tv_shows.id = tv_show_ratings.show_id
