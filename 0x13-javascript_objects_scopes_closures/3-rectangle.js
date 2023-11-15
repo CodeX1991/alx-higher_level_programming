784b83aed93139f5c6ef9beddb1dbac6e1065e2d
CodeX1991 <oghenewonaholy@gmail.com>
2023-11-14 16:37:35 -0800

    Add a print () method

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      while (j < this.width) {
+        if (j === this.width - 1) {
+          console.log('x');
+        } else {
+          process.stdout.write('x');
+        }
+        j++;
+      }
+      i++;
+    }
+  }
+};
