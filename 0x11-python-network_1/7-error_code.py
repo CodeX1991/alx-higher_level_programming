4637a5284db8ae3e653128de8fc2b3618873a252
CodeX1991 <oghenewonaholy@gmail.com>
2024-01-12 12:04:28 -0800

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that takes in a url, sends a request to the url
+and displays the body of the response
+"""
+
+from sys import argv
+import requests
+from urllib.error import HTTPError
+
+# Shouldnot execute when imported
+if __name__ == "__main__":
+    # Request for the url
+    r = requests.get(argv[1])
+
+    if r.status_code >= 400:
+        print("Error code:", r.status_code)
+    else:
+        print(r.text)
