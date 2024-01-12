39abc84c76794bc977d464bfa24985250874c52d
CodeX1991 <oghenewonaholy@gmail.com>
2024-01-12 11:46:55 -0800

    POST an email #1 using requests module

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A script that takes in a url an email,
+sends a POST request to the passed url with
+the email as a parameter, and displays the body
+of the response
+"""
+
+from sys import argv
+import requests
+from urllib.parse import urlencode
+
+# Shouldnot execute when imported
+if __name__ == "__main__":
+    # Create data variable
+    payload = {'email': argv[2]}
+
+    # Request for the url
+    r = requests.post(argv[1], params=payload)
+    print(r)
